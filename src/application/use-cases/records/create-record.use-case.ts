ab97fc0eff2d4f9e60552edd2c37537902d6e2b2
awsxde <ali23khour@gmail.com>
2026-02-13 00:41:29 +0330

    feat: add create record use case

---
+++ src/application/use-cases/records/create-record.use-case.ts
@@ -0,0 +1,46 @@
+import { InputParseError } from '@/src/entities/errors/common';
+import type { Record } from '@/src/entities/models/record';
+import type { IInstrumentationService } from '@/src/application/services/instrumentation.service.interface';
+import type { IRecordsRepository } from '@/src/application/repositories/records.repository.interface';
+
+export type ICreateRecordUseCase = ReturnType<typeof createRecordUseCase>;
+
+export const createRecordUseCase =
+  (
+    instrumentationService: IInstrumentationService,
+    recordsRepository: IRecordsRepository
+  ) =>
+  (
+    input: {
+      description: string;
+      amount: number;
+      type: string;
+      date: string;
+      category: string;
+    },
+    userId: string,
+    tx?: any
+  ): Promise<Record> => {
+    return instrumentationService.startSpan(
+      { name: 'createRecord Use Case', op: 'function' },
+      async () => {
+        if (input.description.length < 4) {
+          throw new InputParseError('Record must be at least 4 chars');
+        }
+
+        const newRecord = await recordsRepository.createRecord(
+          {
+            description: input.description,
+            amount: input.amount,
+            type: input.type,
+            date: input.date,
+            category: input.category,
+            userId,
+          },
+          tx
+        );
+
+        return newRecord;
+      }
+    );
+  };
