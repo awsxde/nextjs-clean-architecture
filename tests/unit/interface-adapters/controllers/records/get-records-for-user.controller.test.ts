dd6fee17db5a2095b6ed8536d63395e078f8591d
awsxde <ali23khour@gmail.com>
2026-02-13 00:46:38 +0330

    feat: add get records for user controller test

---
+++ tests/unit/interface-adapters/controllers/records/get-records-for-user.controller.test.ts
@@ -0,0 +1,90 @@
+import { expect, it } from 'vitest';
+
+import { getInjection } from '@/di/container';
+import { UnauthenticatedError } from '@/src/entities/errors/auth';
+
+const signInUseCase = getInjection('ISignInUseCase');
+const createRecordUseCase = getInjection('ICreateRecordUseCase');
+const getRecordsForUserController = getInjection(
+  'IGetRecordsForUserController'
+);
+
+// A great guide on test names
+// https://www.epicweb.dev/talks/how-to-write-better-test-names
+it('returns users records', async () => {
+  const { session } = await signInUseCase({
+    username: 'one',
+    password: 'password-one',
+  });
+
+  await expect(getRecordsForUserController(session.id)).resolves.toMatchObject(
+    []
+  );
+
+  await createRecordUseCase(
+    {
+      description: 'record-one',
+      amount: 1000,
+      type: 'income',
+      date: '2026-02-08T17:47:31.306Z',
+      category: 'salary',
+    },
+    session.userId
+  );
+  await createRecordUseCase(
+    {
+      description: 'record-two',
+      amount: 2000,
+      type: 'income',
+      date: '2026-02-08T17:47:31.306Z',
+      category: 'salary',
+    },
+    session.userId
+  );
+  await createRecordUseCase(
+    {
+      description: 'record-three',
+      amount: 3000,
+      type: 'income',
+      date: '2026-02-08T17:47:31.306Z',
+      category: 'salary',
+    },
+    session.userId
+  );
+
+  await expect(getRecordsForUserController(session.id)).resolves.toMatchObject([
+    {
+      description: 'record-one',
+      amount: 1000,
+      type: 'income',
+      date: '2026-02-08T17:47:31.306Z',
+      category: 'salary',
+      userId: '1',
+    },
+    {
+      description: 'record-two',
+      amount: 2000,
+      type: 'income',
+      date: '2026-02-08T17:47:31.306Z',
+      category: 'salary',
+      userId: '1',
+    },
+    {
+      description: 'record-three',
+      amount: 3000,
+      type: 'income',
+      date: '2026-02-08T17:47:31.306Z',
+      category: 'salary',
+      userId: '1',
+    },
+  ]);
+});
+
+it('throws when unauthenticated', () => {
+  expect(getRecordsForUserController('')).rejects.toBeInstanceOf(
+    UnauthenticatedError
+  );
+  expect(getRecordsForUserController(undefined)).rejects.toBeInstanceOf(
+    UnauthenticatedError
+  );
+});
