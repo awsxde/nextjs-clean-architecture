8ac0fe427e7704ef085ce171a001b9f11ce05600
awsxde <ali23khour@gmail.com>
2026-02-17 02:56:33 +0330

    fix: get records import

--- app/dashboard/record/page.tsx
+++ app/dashboard/record/page.tsx
@@ -1,51 +1,12 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-
-import { SESSION_COOKIE } from '@/config';
-import {
-  AuthenticationError,
-  UnauthenticatedError,
-} from '@/src/entities/errors/auth';
 import { Record } from '@/src/entities/models/record';
-import { getInjection } from '@/di/container';
 import { columns } from '@/app/dashboard/record/components/columns';
 import { DataTable } from '@/app/dashboard/record/components/data-table';
-
-async function getRecords(sessionId: string | undefined) {
-  const instrumentationService = getInjection('IInstrumentationService');
-  return await instrumentationService.startSpan(
-    {
-      name: 'getRecords',
-      op: 'function.nextjs',
-    },
-    async () => {
-      try {
-        const getRecordsForUserController = getInjection(
-          'IGetRecordsForUserController'
-        );
-        return await getRecordsForUserController(sessionId);
-      } catch (err) {
-        if (
-          err instanceof UnauthenticatedError ||
-          err instanceof AuthenticationError
-        ) {
-          redirect('/sign-in');
-        }
-        const crashReporterService = getInjection('ICrashReporterService');
-        crashReporterService.report(err);
-        throw err;
-      }
-    }
-  );
-}
+import { getRecords } from './actions';
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  const sessionId = cookieStore.get(SESSION_COOKIE)?.value;
-
   let records: Record[];
   try {
-    records = await getRecords(sessionId);
+    records = await getRecords();
   } catch (err) {
     throw err;
   }
