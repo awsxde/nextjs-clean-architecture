1ec3583fb2926889c43abe18e2a69e8fc7f4e574
awsxde <ali23khour@gmail.com>
2026-02-17 00:24:41 +0330

    fix: transaction type in record repository interface

--- src/application/repositories/records.repository.interface.ts
+++ src/application/repositories/records.repository.interface.ts
@@ -1,13 +1,14 @@
 import type { Record, RecordInsert } from '@/src/entities/models/record';
+import type { ITransaction } from '@/src/entities/models/transaction.interface';
 
 export interface IRecordsRepository {
-  createRecord(record: RecordInsert, tx?: any): Promise<Record>;
+  createRecord(record: RecordInsert, tx?: ITransaction): Promise<Record>;
   getRecord(id: number): Promise<Record | undefined>;
   getRecordsForUser(userId: string): Promise<Record[]>;
   updateRecord(
     id: number,
     input: Partial<RecordInsert>,
-    tx?: any
+    tx?: ITransaction
   ): Promise<Record>;
-  deleteRecord(id: number, tx?: any): Promise<void>;
+  deleteRecord(id: number, tx?: ITransaction): Promise<void>;
 }
