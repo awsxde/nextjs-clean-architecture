1b2fe398ee85fd5e90df025b863647006f807f2f
awsxde <ali23khour@gmail.com>
2026-02-17 16:17:17 +0330

    fix: quick create link in sidebar

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -26,9 +26,12 @@ export function NavMain({
             <SidebarMenuButton
               tooltip="Quick Create"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+              asChild
             >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
+              <Link href="/dashboard/record/new">
+                <IconCirclePlusFilled />
+                <span>Quick Create</span>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
