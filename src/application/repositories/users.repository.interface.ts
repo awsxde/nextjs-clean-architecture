e3765e93e7698595fbe143cfb6aa5d970819420b
awsxde <ali23khour@gmail.com>
2026-02-19 00:38:17 +0330

    feat: add new repository functions

--- src/application/repositories/users.repository.interface.ts
+++ src/application/repositories/users.repository.interface.ts
@@ -1,8 +1,21 @@
-import type { User, CreateUser } from '@/src/entities/models/user';
+import type {
+  User,
+  CreateUser,
+  CreateOAuthUser,
+} from '@/src/entities/models/user';
 import type { ITransaction } from '@/src/entities/models/transaction.interface';
 
 export interface IUsersRepository {
   getUser(id: string): Promise<User | undefined>;
   getUserByUsername(username: string): Promise<User | undefined>;
   createUser(input: CreateUser, tx?: ITransaction): Promise<User>;
+  getUserByEmail(email: string): Promise<User | undefined>;
+  getUserByGithubId(githubId: string): Promise<User | undefined>;
+  getUserByGoogleId(googleId: string): Promise<User | undefined>;
+  updateUser(
+    userId: string,
+    data: Partial<User>,
+    tx?: ITransaction
+  ): Promise<User>;
+  createOAuthUser(input: CreateOAuthUser, tx?: ITransaction): Promise<User>; // optional helper
 }
