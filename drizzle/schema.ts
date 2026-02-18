5b53a3353318c6af1dc626eb16257da83008ef55
awsxde <ali23khour@gmail.com>
2026-02-19 00:36:38 +0330

    fix: change user schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -3,7 +3,10 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 export const users = sqliteTable('user', {
   id: text('id').primaryKey(),
   username: text('username').notNull(),
-  password_hash: text('password_hash').notNull(),
+  email: text('email').notNull().unique(),
+  password_hash: text('password_hash'),
+  github_id: text('github_id').unique(),
+  google_id: text('google_id').unique(),
 });
 
 export const sessions = sqliteTable('session', {
